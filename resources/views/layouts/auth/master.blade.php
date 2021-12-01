b1b81be3715db7ec569d26b95d1bdcaf337d73f0
mdaasim <work.aasim@gmail.com>
2021-12-01 20:16:28 +0530

    frontend changes

--- resources/views/layouts/auth/master.blade.php
+++ resources/views/layouts/auth/master.blade.php
@@ -81,8 +81,8 @@
         </nav>
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4 side-gradient">
-            <a href="{{ route('home') }}" class="brand-link">
-                <img src="{{ asset('admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <a href="{{ route('home') }}" class="brand-link d-flex flex-column align-items-center">
+                <img src="{{ asset('admin/dist/img/money-bag.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">{{env('APP_NAME')}} Admin</span>
             </a>
 
